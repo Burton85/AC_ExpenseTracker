edcfb4a2f9ee263f545ececb402e439e0cfcd064
Burton85 <burton00tw@gmail.com>
2019-12-04 13:02:05 +0800

    fix syna from Layout

--- app.js
+++ app.js
@@ -61,7 +61,6 @@ require("./config/passport.js")(passport);
 app.use(flash());
 
 app.use((req, res, next) => {
-  app.locals.layout = "main";
   res.locals.user = req.user;
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.success_msg = req.flash("success_msg").toString();
