5b4651852a6070abf26a7c5ae46263592ed16c13
Burton85 <burton00tw@gmail.com>
2019-12-04 13:09:47 +0800

    Add path for view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const recordDB = require("./models/records.js");
 const session = require("express-session");
 const passport = require("passport");
 const flash = require("connect-flash");
+const path = require("path");
 
 //DISTINGUISH THE ENVIRONMENT
 require("dotenv").config();
@@ -16,8 +17,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //import method override
 app.use(methodOverride("_method"));
 //connect with view
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    extname: "handlebars",
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "views/partials"),
+    layoutsDir: path.join(__dirname, "views/layouts")
+  })
+);
 app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(express.static("public"));
 
 //connect with model
@@ -61,7 +72,7 @@ require("./config/passport.js")(passport);
 app.use(flash());
 
 app.use((req, res, next) => {
-  app.locals.layout = "/views/layouts/main.handlebars";
+  // app.locals.layout = false;
   res.locals.user = req.user;
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.success_msg = req.flash("success_msg").toString();
