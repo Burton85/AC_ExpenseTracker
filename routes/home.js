0b6d3460b16d26577f96e46cf2dce054dd5cc95e
Burton85 <burton00tw@gmail.com>
2019-12-04 18:19:45 +0800

    Sort by month

--- routes/home.js
+++ routes/home.js
@@ -8,42 +8,74 @@ const dateFormat = require("dateformat");
 router.get("/", authenticated, (req, res) => {
   var now = new Date();
   let category = req.query.category;
+  let month = req.query.month;
+  let recordResult = [];
   let totalAmount = 0;
+
   if (category != null) {
     recordDB
       .find({ userId: req.user._id, category: category })
       .exec((err, records) => {
         if (err) return console.error(err);
-        records.forEach(item => {
+
+        //過濾月份
+        if (month != null) {
+          recordResult = records.filter(
+            item => dateFormat(item.date, "mm") === month
+          );
+        } else {
+          recordResult = records;
+        }
+
+        recordResult.forEach(item => {
+          //轉換日期
           item.formated_date = dateFormat(item.date, "isoDate");
+          //計算總金額
           totalAmount += Number(item.amount);
+          //判斷icon
           if (item.category == "家居物業") item.home = true;
           else if (item.category == "交通出行") item.bus = true;
           else if (item.category == "休閒娛樂") item.smile = true;
           else if (item.category == "餐飲食品") item.food = true;
           else item.pen = true;
         });
+
         return res.render("index", {
-          records: records,
+          records: recordResult,
           category: category,
-          totalAmount: totalAmount
+          totalAmount: totalAmount,
+          month: month
         });
       });
   } else {
     recordDB.find({ userId: req.user._id }).exec((err, records) => {
       if (err) return console.error(err);
-      records.forEach(item => {
+
+      //過濾月份
+      if (month != null) {
+        recordResult = records.filter(
+          item => dateFormat(item.date, "mm") === month
+        );
+      } else {
+        recordResult = records;
+      }
+      recordResult.forEach(item => {
+        //轉換日期
         item.formated_date = dateFormat(item.date, "isoDate");
+        //計算總金額
         totalAmount += Number(item.amount);
+        //判斷icon
         if (item.category == "家居物業") item.home = true;
         else if (item.category == "交通出行") item.bus = true;
         else if (item.category == "休閒娛樂") item.smile = true;
         else if (item.category == "餐飲食品") item.food = true;
         else item.pen = true;
       });
+
       return res.render("index", {
-        records: records,
-        totalAmount: totalAmount
+        records: recordResult,
+        totalAmount: totalAmount,
+        month: month
       });
     });
   }
